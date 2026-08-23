bcbf61c93db28b5f007a1e2819a6d4eff2cd38dd
Marco Hoffmann <261466438+McCryptoX@users.noreply.github.com>
2026-08-23 18:04:31 +0200

    Switch entity_id to sensor.tarifvergleich_preis_fix with fallback in dashboard

--- custom_components/energy_tariff_compare/sensor.py
+++ custom_components/energy_tariff_compare/sensor.py
@@ -28,7 +28,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
             PricesSensor(hass),
             PriceNowSensor(hass, "octopus_heat", "tarifvergleich_preis_heat", "Octopus Heat"),
             PriceNowSensor(hass, "octopus_heat_loyalty", "tarifvergleich_preis_loyalty", "Heat Loyalty"),
-            PriceNowSensor(hass, "naturwerke_fix", "tarifvergleich_preis_naturwerke", "Fixer Tarif"),
+            PriceNowSensor(hass, "naturwerke_fix", "tarifvergleich_preis_fix", "Fixer Tarif"),
             PriceNowSensor(hass, "dynamic", "tarifvergleich_preis_dynamisch", "Dynamisch"),
             PriceNowSensor(hass, "dynamic_modul3", "tarifvergleich_preis_modul3", "Dynamisch + Modul 3"),
             PeriodSensor(hass, "today", "Heute", "tarifvergleich_heute"),
